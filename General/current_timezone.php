496e297014e29793f51b2e3a127bf6517ef85dc7
Alessandra Nicole Santos <152505343+alessandranicolesantos@users.noreply.github.com>
2024-12-17 18:22:02 +0800

    Update current_timezone.php

--- General/current_timezone.php
+++ General/current_timezone.php
@@ -1,1 +1,10 @@
+<?php
+// Set the default timezone to Manila, Philippines
+date_default_timezone_set('Asia/Manila');
 
+// Get the current date and time
+$currentDateTime = date('l, F j, Y h:i:s A'); // Format: Day, Month Date, Year Hour:Minute:Second AM/PM
+
+// Output the current date and time
+echo $currentDateTime;
+?>
